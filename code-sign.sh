2dbfa4b1b5025fb85086c5d10e443680cc750b9e
Kyle Ericson <kyleri@kyleri-jgh5.truemfg.com>
2019-08-09 14:52:37 -0500

    Update code-sign.sh
    made the read me easier to read

--- code-sign.sh
+++ code-sign.sh
@@ -2,26 +2,23 @@
 #created by Kyle Ericson
 #Date Aug 9th 2019
 #Version 1.0
-#Read me for: code-sign.sh
-#This is a easy to use GUI scipt for codesigning .pkg files
-#Update the script with your dev ID
-#Then use this script to make it into a .app
-#https://gist.github.com/mathiasbynens/674099#file-appify
-
+#Purpose: Easy to use GUI app to Code Sign PKG files with a Apple Developer ID Installer cert
+#Requirements: 
+#1.	A Apple Developer ID Installer cert must be installed on the current machine.
+#2.	Script to make into .app file     https://gist.github.com/mathiasbynens/674099#file-appify
+#3.	Update the Script with your dev ID in the section below
 
 consoleUser=$(stat -f %Su /dev/console)
-
 file=`osascript -e 'tell app (path to frontmost application as Unicode text) to set new_file to POSIX path of (choose file with prompt "Pick a PKG file to CodeSign" of type {"PKG"})'  2> /dev/null`
 newname=$(/usr/bin/osascript -e 'Tell application "System Events" to display dialog "Please enter a new name for your signed package.\n\nNote: The signed PKG will be saved your desktop.\n\nImportant: .pkg must be at the end of your filename!" default answer "signed.pkg"' -e 'text returned of result' 2>/dev/null)
-echo $file
-echo $newname
-echo $new_file
+
+####################################################
 #Replace this line with your Developer ID Installer:
 productsign --sign 'Developer ID Installer: Mordo Inc. (AAA111AA11)' ${file} /Users/$consoleUser/Desktop/${newname}
+####################################################
 
-Result=`/usr/sbin/pkgutil --check-signature "$newname"`
-echo $Result
 
+#Result=`/usr/sbin/pkgutil --check-signature "$newname"`
 
 #  "Message"
 function msg() {
