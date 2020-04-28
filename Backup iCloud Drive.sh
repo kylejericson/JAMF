c77d0acbff065860eb4bf7e142c00ae290184edd
kylejericson <43736908+kylejericson@users.noreply.github.com>
2020-04-27 23:54:11 -0500

    Create Backup iCloud Drive.sh

---
+++ Backup iCloud Drive.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+#Source: https://www.jessesquires.com/blog/2019/09/27/icloud-backup-using-rsync/
+#Used for copying iCloud folder to current user's desktop
+#Deploy with Jamf Pro
+#Created by Kyle Ericson
+
+USER=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+
+DEST="/Users/$USER/Desktop/iCloud-Backup/"
+
+SRC="/Users/$USER/Library/Mobile Documents/com~apple~CloudDocs/"
+
+rsync --verbose --recursive --delete-before --whole-file --times --exclude=".DS_Store" --exclude=".Trash/" "$SRC" "$DEST"
+
+sudo chown -Rv $USER /Users/$USER/Desktop/iCloud-Backup
+
+exit 0
