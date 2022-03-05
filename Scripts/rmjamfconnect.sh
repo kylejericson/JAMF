ea5d74a166b2b7913e1cec2653662311d6d2b87f
Kyle Ericson <43736908+kylejericson@users.noreply.github.com>
2022-03-04 23:25:43 -0600

    4.0

--- Scripts/rmjamfconnect.sh
+++ Scripts/rmjamfconnect.sh
@@ -1,18 +1,24 @@
 #!/bin/zsh
 #	This will uninstall Jamf Connect and reset the login window
 # 	Created by Kyle Ericson
-#	Version 3.0
+#	Version 4.0
+echo "Created by Kyle Ericson"
+echo "email kyle@ericsontech.com"
 
 # Get the logged in user's name
 FAKE_USER=$(/bin/echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/&&!/loginwindow/{print $3}')
 CURRENT_USER=$(id -un $FAKE_USER)
+CURRENT_USER_HOME=$(dscacheutil -q user | grep -A 3 -B 2 $CURRENT_USER | grep dir | awk '{print $2}')
+Plist="$CURRENT_USER_HOME/Library/LaunchAgents/com.jamf.connect.plist"
 
+# Reset login window to default macOS
 /usr/local/bin/authchanger -reset
 rm /usr/local/bin/authchanger
 rm /usr/local/lib/pam/pam_saml.so.2
 rm -r /Library/Security/SecurityAgentPlugins/JamfConnectLogin.bundle
-launchctl bootout /Users/$CURRENT_USER/Library/LaunchAgents/com.jamf.connect.plist
-#launchctl unload /Users/$CURRENT_USER/Library/LaunchAgents/com.jamf.connect.plist
+
+# Unload the Jamf Connect LaunchAgent
+/bin/launchctl asuser $CURRENT_USER /bin/launchctl unload "$Plist"
 rm -rf /Users/$CURRENT_USER/Library/LaunchAgents/com.jamf.connect.plist
 killall 'Jamf Connect'
 rm -rf "/Applications/Jamf Connect.app"
