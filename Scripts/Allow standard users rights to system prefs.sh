f01c4d1862c0226b97d3cd5d60052b8439253ad7
Kyle Ericson <43736908+kylejericson@users.noreply.github.com>
2022-03-19 22:38:41 -0500

    new

---
+++ Scripts/Allow standard users rights to system prefs.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/sh
+# Created by Kyle Ericson
+
+# unlock the sysprefs before unlocking specific panes: 
+security authorizationdb write system.preferences allow 
+
+# unlock energysaver: 
+security authorizationdb write system.preferences.energysaver allow
+
+# unlock datetime:
+security authorizationdb write system.preferences.datetime allow
+
+# unlock printing:
+security authorizationdb write system.preferences.printing allow
+
+# unlock network:
+security authorizationdb write system.preferences.network allow
+
+# add staff to lpadmin group
+/usr/sbin/dseditgroup -o edit -t group -a staff _lpadmin
+
+exit 0
