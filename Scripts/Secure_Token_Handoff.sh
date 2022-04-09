066249b311aa2652ec426b2c31fa319ea184d289
Kyle Ericson <43736908+kylejericson@users.noreply.github.com>
2022-04-09 00:23:33 -0500

    removed python from code
    Removed python for macOS 12.3.1

--- Scripts/Secure_Token_Handoff.sh
+++ Scripts/Secure_Token_Handoff.sh
@@ -131,7 +131,7 @@ fi
 
 
 				# Get the current logged in user
-				userName=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+				userName=$(/bin/echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/&&!/loginwindow/{print $3}')
 
 				# Check if end user is admin
 
