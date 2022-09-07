17b074e32e4e1f537c66bf8d0041b377b33136a2
Kyle Ericson <43736908+kylejericson@users.noreply.github.com>
2022-09-07 15:11:36 -0500

    new script

---
+++ Scripts/Grant Perm Admin Rights.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+# Created by Kyle Ericson
+# Removes admin rights from current user
+
+U1=$(/bin/echo "show State:/Users/ConsoleUser" | /usr/sbin/scutil | /usr/bin/awk '/Name :/&&!/loginwindow/{print $3}')
+U2=$(id -un $U1)
+echo "Current username is:$U2"
+/usr/sbin/dseditgroup -o edit -a $U2 -t user admin
+dscl . -append /groups/admin GroupMembership $U2
+exit 0
