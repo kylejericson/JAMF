2822db247b0dbc02c739c173f15fc8358a474bc1
Kyle Ericson <43736908+kylejericson@users.noreply.github.com>
2023-02-17 14:41:17 -0600

    Update Set-Account-Picture-via-Jamf-Connect.sh
    testing fixes

--- Scripts/Set-Account-Picture-via-Jamf-Connect.sh
+++ Scripts/Set-Account-Picture-via-Jamf-Connect.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 # Created by Kyle Ericson
-# Make sure to check your token for the right value for $EMAIL I set mine to upn line 9
-# Update Line 14 with your Azure Storage blob
+# Updated by ChatGPT AI for desktop
 
-USR=$(dscl . list /Users | grep -v '^_' | grep -v 'root' | grep -v 'nobody' | grep -v 'daemon' | grep -v 'ericsontechadmin' | grep -v '/')
-#AL=$(dscl . read /Users/$USR RecordName | grep -v "com.apple.idms.appleid.prd*")
+# Make sure the token file exists
 TOKEN_BASIC="/private/tmp/token"
-EMAIL=$(echo "$(cat $TOKEN_BASIC)" | sed -e 's/\"//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep upn | cut -d ":" -f2)
-EMAIL=$(echo "$EMAIL" | sed 's/}//')
-echo "Setting account picture for user:$EMAIL"
+if [ ! -f "$TOKEN_BASIC" ]; then
+  echo "Error: Token file not found"
+  exit 1
+fi
+
+# Get the email address from the token
+EMAIL=$(jq -r '.upn' < "$TOKEN_BASIC")
+if [ -z "$EMAIL" ]; then
+  echo "Error: Could not retrieve email address from token"
+  exit 1
+fi
+
+echo "Setting account picture for user: $EMAIL"
 
 # Download the images
-curl -L "https://myazureblobname.blob.core.windows.net/mdm/$EMAIL.png" -o /tmp/$EMAIL.png
+if ! curl -L "https://myazureblobname.blob.core.windows.net/mdm/$EMAIL.png" -o "/tmp/$EMAIL.png"; then
+  echo "Error downloading image for user $EMAIL"
+  exit 1
+fi
 
-#Convert the Image to tiff
-sips -s format tiff /tmp/$EMAIL.png --out /tmp/$EMAIL.tiff
+# Convert the image to TIFF format
+if ! sips -s format tiff "/tmp/$EMAIL.png" --out "/tmp/$EMAIL.tiff"; then
+  echo "Error converting image for user $EMAIL"
+  exit 1
+fi
 
-#Remove Old Images
-dscl . delete /Users/$USR JPEGPhoto
-
-#Set Images
+# Set the user's picture
+USR=$(id -u)
 dscl . create /Users/$USR Picture "/tmp/$EMAIL.tiff"
 
 exit 0
