48599d4a6877f3725631edd2784085f4db318cfb
Kyle Ericson <43736908+kylejericson@users.noreply.github.com>
2024-06-13 10:18:07 -0500

    3.0

--- Scripts/JamfConnectNotifyStart.sh
+++ Scripts/JamfConnectNotifyStart.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # Created by Kyle Ericson
-# Version 2.0
+# Version 3.0
 # Jamf Connect Notify Start
 # Note you will need to ensure Jamf Connect is set to run in Notify Mode like this:
 # /usr/local/bin/authchanger -reset -JamfConnect -Notify
@@ -9,6 +9,7 @@
 # Caffeinate Mac to keep awake
 /usr/bin/caffeinate -d -i -m -u & caffeinatePID=$!
 
+# Quit Key set to command + control + x
 
 ORG_NAME="My Org Name"
 BANNER_IMAGE_PATH="/Library/Resources/logo.png"
@@ -24,36 +25,35 @@ POLICY_ARRAY=(
     "Setting up your Mac 80% Complete...,outlook"
     "Setting up your Mac 90% Complete...,powerpoint"
     "Setting up your Mac 99% Complete...,word"
-    
-  )
-  
+)
+
 ARAY_LENGTH="${#POLICY_ARRAY[@]}"
 for (( index = 1; index <= count; index ++ )); do
   echo "${index} of ${count}: ${POLICY_ARRAY[index]}"
-done 
+done
 
-#ARAY_LENGTH="$((${#POLICY_ARRAY[@]}))"
-echo "STARTING RUN" >> $NOTIFY_LOG # Define the number of increments for the progress bar
-echo "Command: Image: $BANNER_IMAGE_PATH" >> $NOTIFY_LOG
-echo "Command: MainTitle: Installing Apps and Settings." >> $NOTIFY_LOG
-echo "Command: MainText: Thanks for choosing a Mac at $ORG_NAME! We want you to have a few applications and settings configured before you get started with your new Mac. This process should take 10 to 20 minutes to complete. \n \n If you need additional software or help, please visit the Self Service app in your Applications folder or on your Dock." >> $NOTIFY_LOG
-echo "Command: DeterminateManual: $ARAY_LENGTH" >> $NOTIFY_LOG
+echo "STARTING RUN" >> "$NOTIFY_LOG" # Define the number of increments for the progress bar
+echo "Command: Image: $BANNER_IMAGE_PATH" >> "$NOTIFY_LOG"
+echo "Command: MainTitle: Installing Apps and Settings." >> "$NOTIFY_LOG"
+echo "Command: MainText: Thanks for choosing a Mac at $ORG_NAME! We want you to have a few applications and settings configured before you get started with your new Mac. This process should take 10 to 20 minutes to complete. \n \n If you need additional software or help, please visit the Self Service app in your Applications folder or on your Dock." >> "$NOTIFY_LOG"
+echo "Command: DeterminateManual: $ARAY_LENGTH" >> "$NOTIFY_LOG"
 
 # Loop to run policies
-  for POLICY in "${POLICY_ARRAY[@]}"; do
-    echo "Status: $(echo "$POLICY" | cut -d ',' -f1)" >> $NOTIFY_LOG
-      /usr/local/bin/jamf policy -event "$(echo "$POLICY" | cut -d ',' -f2)"
-      echo "Command: DeterminateManualStep: ${POLICY_ARRAY[index]}" >> $NOTIFY_LOG
+for POLICY in "${POLICY_ARRAY[@]}"; do
+    echo "Status: $(echo "$POLICY" | cut -d ',' -f1)" >> "$NOTIFY_LOG"
+    /usr/local/bin/jamf policy -event "$(echo "$POLICY" | cut -d ',' -f2)"
+    echo "Command: DeterminateManualStep: ${POLICY_ARRAY[index]}" >> "$NOTIFY_LOG"
 done
+
 sleep 5
 
-###Clean Up
+### Clean Up
 sleep 3
-echo "Command: Quit" >> $NOTIFY_LOG
+echo "Command: Quit" >> "$NOTIFY_LOG"
 sleep 1
-rm -rf $NOTIFY_LOG
+rm -rf "$NOTIFY_LOG"
  
-#Disable notify screen from loginwindow process and remove script
+# Disable notify screen from loginwindow process and remove script
 /usr/local/bin/authchanger -reset -JamfConnect
 rm -rf /usr/local/bin/start-jcnotify.sh
 
